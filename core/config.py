8ae8d67b1a113a93043c29979697d1b51441cf2a
naman884 <namangupta488@gmail.com>
2023-03-25 23:10:46 +0530

    update

--- core/config.py
+++ core/config.py
@@ -1,6 +1,26 @@
+# below libraries needed to load postgres variables from the ".env" file
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_path = Path('.') / '.env'  # <-- this means goto the myWork dir and search for .env file there
+
+load_dotenv(dotenv_path = env_path)
+
+#--------------------------------------------------------------------------------------------------------#
+
 class Settings:
-    PROJECT_TITLE: str = 'Jobboard'
-    PROJECT_VERSION: str = '0.1.0'
+    PROJECT_TITLE : str = 'Jobboard'
+    PROJECT_VERSION : str = '0.1.0'
+
+    POSTGRES_USER : str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER", "localhost")
+    POSTGRES_PORT : str = os.getenv("POSTGRES_PORT", 5432)
+    POSTGRES_DB : str = os.getenv("POSTGRES_DB", "fastapi_course_db")
+    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+
 
 # now we will create object of Settings class
 settings = Settings()
--- main.py
+++ main.py
@@ -3,11 +3,22 @@ from fastapi import FastAPI
 # now we will import object from config.py file into this file
 from core.config import settings
 
+from db.session import engine
+from db.base_class import Base
+
 #----------------------------------------------------------------------------------------------------------#
 
-app = FastAPI(title = settings.PROJECT_TITLE, version = settings.PROJECT_VERSION)
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+def start_application():
+    app = FastAPI(title = settings.PROJECT_TITLE, version = settings.PROJECT_VERSION)
+    create_tables()
+    return app
 
 # now we will use this "app" object to create different routes/endpoints/urls which our users will hit
+app = start_application()
+
 
 # creating a "GET" route
 @app.get('/')
